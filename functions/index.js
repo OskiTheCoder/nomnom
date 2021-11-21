e064e5c8b937b2309ed211bb4c63bca63b7a3218
shelvy <sks91@berkeley.edu>
2021-11-20 21:08:58 -0800

    express

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.get('/hithere', (req, res) => {
+  res.json({ message: 'hello from a cloud function' });
 });
+
+exports.test = functions.https.onRequest(app);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [hello, setHello] = useState('');
+  const getHello = async () => {
+    try {
+      const response = await fetch(
+        'https://us-central1-nomnom-13317.cloudfunctions.net/test/hithere'
+      );
+      const jsonData = await response.json();
+      console.log(jsonData);
+
+      setHello(jsonData.message);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getHello();
+  }, []);
   return (
     <div className='App'>
-      <header className='App-header'>hello</header>
+      <header className='App-header'>
+        hello there and {hello} !!! {`🤓`}
+      </header>
     </div>
   );
 }
