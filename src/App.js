72e51c7b41e64bbd709e37adcef1d01c88717023
shelvy <sks91@berkeley.edu>
2021-11-26 17:43:55 -0800

    sass

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
-  const [hello, setHello] = useState('');
-  const getHello = async () => {
-    try {
-      const response = await fetch(
-        'https://us-central1-nomnom-13317.cloudfunctions.net/test/hithere'
-      );
-      const jsonData = await response.json();
-      console.log(jsonData);
-
-      setHello(jsonData.message);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  useEffect(() => {
-    getHello();
-  }, []);
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        hello there and {hello} !!! {`🤓`}
-      </header>
-    </div>
-  );
+  return <>hello</>;
 }
 
 export default App;
