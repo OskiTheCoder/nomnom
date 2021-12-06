358852396875d42b52dd02512f3fd7b57efb16fe
shelvy <sks91@berkeley.edu>
2021-12-05 18:48:24 -0800

    add more content to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,8 +6,17 @@ const Main = () => {
     <main className='main-container'>
       <div className='main-container__header'>
         <h1 className='main-container__header--heading'>
-          a modern way to share, upload, and discover new recipes
+          Finally. A modern way to share, upload, and discover new recipes.
         </h1>
+        <p className='main-container__header--paragraph'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          mollitia incidunt obcaecati minima atque enim quaerat velit ratione
+          quidem impedit recusandae corrupti neque at natus suscipit, odit
+          numquam?
+        </p>
+        <button className='main-container__header--explore-button'>
+          Explore
+        </button>
       </div>
       <div className='main-container__img'></div>
     </main>
